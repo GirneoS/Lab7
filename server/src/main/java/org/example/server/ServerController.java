f94f813631b973c100f871ff5848cb19b70e0605
GirS <vntskegr@gmail.com>
2024-05-23 19:20:37 +0500

    smth

--- server/src/main/java/org/example/server/ServerController.java
+++ server/src/main/java/org/example/server/ServerController.java
@@ -7,5 +7,6 @@ public class ServerController {
     public static void main(String[] args) {
         logger.info("The server has just start!\n-------------------------------------------------\n");
 
+
     }
 }
