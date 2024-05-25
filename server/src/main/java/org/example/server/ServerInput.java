6b54363a1bc8e6dc0b8ecd3dcf792277c0a3dca7
GirneoS <84183978+GirneoS@users.noreply.github.com>
2024-05-25 15:16:46 +0300

    Update ServerInput.java

--- server/src/main/java/org/example/server/ServerInput.java
+++ server/src/main/java/org/example/server/ServerInput.java
@@ -8,7 +8,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Logger;
 
-public class ServerInput implements Runnable{
+public class ServerInput{
     private static final Logger logger = Logger.getLogger("Laba7");
     private DatagramChannel channel;
 
